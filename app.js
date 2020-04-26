10a0ca43e10a204ba1dd0232363a78670291b66c
amirali <amirali.mirian@gmail.com>
2020-04-26 22:01:20 +0430

    completed the template and completed the app.js.

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
+const express = require('express');
 const axios = require('axios');
+const ejs = require('ejs');
 
 const url = "http://covid19api.xapix.io/v2/locations";
+const port = process.env.PORT || 3000;
 
-axios.get(url).then(resp => {
-    console.log('got the data.');
-    return resp.data;
-}).then(data => {
-    console.log(data.latest);
-}).catch(function (error) {
-    console.log('error in getting the data.');
+let app = express();
+app.set('view engine', 'ejs');
+app.get('/', function (req, res) {
+    axios.get(url).then(resp => {
+        console.log('got the data.');
+        return resp.data;
+    }).then(data => {
+        res.render('home', {locations: data.locations, latest: data.latest});
+    }).catch(function (error) {
+        console.log('error in getting the data.');
+    });
 });
+
+app.listen(port);
+console.log(`listening on port ${port}`);
